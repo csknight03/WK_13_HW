16b1d08c75e7ed496a8a070f646c4b11e6d2dbdb
Craig Knight <csk0330@outlook.com>
2018-03-10 05:24:19 -0700

    first commit

---
+++ data/friends.js
@@ -0,0 +1,71 @@
+//Disney characters
+
+var friends = [{
+    name: "Mickey Mouse",
+    photo: "https://upload.wikimedia.org/wikipedia/hr/0/06/Miki_Maus.jpg",
+    scores: [5, 5, 3, 5, 5, 1, 5, 5, 5, 2]
+}, {
+    name: "Minnie Mouse",
+    photo: "http://www.sablonynazed.cz/343-large_default/minnie-01.jpg",
+    scores: [5, 5, 3, 5, 5, 1, 5, 5, 5, 2]
+}, {
+    name: "Goofy",
+    photo: "https://family.disney.com/wp-content/uploads/2012/03/goofy-word-search-420x420.jpg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+}, {
+    name: "Daisy Duck",
+    photo: "https://images.homedepot-static.com/productImages/480700b0-6c91-4106-b34e-d31c76c6fe2e/svn/whites-roommates-wall-decals-rmk1513gm-64_1000.jpg",
+    scores: [5, 5, 3, 5, 5, 1, 5, 5, 5, 2]
+}, {
+    name: "Donald Duck",
+    photo: "https://vignette.wikia.nocookie.net/scrooge-mcduck/images/a/ae/Donald_Duck_New_Image.jpg/revision/latest?cb=20161217135721",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+}, {
+    name: "Belle",
+    photo: "https://media1.popsugar-assets.com/files/thumbor/UOmPJ3E-3xFRE5VF18c04lvzQIM/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2017/02/15/845/n/1922283/787dd66f58a4a9688dd0a6.57229241_edit_img_image_17053399_1487185029/i/Beauty-Beast-Behind--Scenes-Clip.jpg",
+    scores: [5, 5, 2, 2, 5, 1, 3, 3, 4, 2]
+}, {
+    name: "Ariel",
+    photo: "https://temporarytattoos.com/pub/media/catalog/product/cache/image/700x560/e9c3970ab036de70892d86c6d221abfe/a/r/ariel-from-little-mermaid-temporary-tattoo_dsny-sm-01.jpg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+}, {
+    name: "Winnie the Pooh",
+    photo: "https://metrouk2.files.wordpress.com/2016/07/elib_7085634.jpg?w=620&h=620&crop=1",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+    name: "Simba",
+    photo: "https://vignette.wikia.nocookie.net/disney/images/e/ed/TLK_Simba.png/revision/latest?cb=20120730040249",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+    name: "Princess Jasmine",
+    photo: "https://s-media-cache-ak0.pinimg.com/originals/57/5c/42/575c4224b904a905b4b13ef87bb25a90.jpg",
+    scores: [5, 5, 2, 2, 5, 1, 3, 3, 4, 2]
+}, {
+    name: "Gaston",
+    photo: "https://vignette.wikia.nocookie.net/yandere/images/d/de/Gaston.png/revision/latest?cb=20150202022244",
+    scores: [2, 1, 4, 5, 1, 1, 4, 4, 5, 4]
+}, {
+    name: "Jafar",
+    photo: "http://editorial.designtaxi.com/editorial-images/News-Twitter-Has-A-Huge-Crush-On-New-Jafar-Actor-Of-Disneys-Live-Action-Aladdin-090717/News-Twitter-Has-A-Huge-Crush-On-New-Jafar-Actor-Of-Disneys-Live-Action-Aladdin-090717-1.jpeg",
+    scores: [2, 1, 4, 5, 1, 1, 4, 4, 5, 4]
+}, {
+    name: "Hades",
+    photo: "https://img00.deviantart.net/509c/i/2011/272/a/5/hades_by_defiantprincess21-d4bcbpg.jpg",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+    name: "Scar",
+    photo: "https://lumiere-a.akamaihd.net/v1/images/character_thelionking_scar_68f3f708.jpeg?region=0,0,450,450",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+}, {
+    name: "Captain Hook",
+    photo: "https://pbs.twimg.com/profile_images/829240167303090178/3k6k2jsg_400x400.jpg",
+    scores: [2, 1, 4, 5, 1, 1, 4, 4, 5, 4]
+}, {
+    name: "Oogie Boogie",
+    photo: "https://i5.walmartimages.com/asr/c998742d-a20b-4933-95b4-42155c34633b_1.aca3d5170aed9610920a33f541558374.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+    scores: [1, 1, 1, 1, 1, 5, 1, 1, 1, 1]
+}];
+
+//export array
+
+module.exports = friends;
